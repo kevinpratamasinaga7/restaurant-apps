21aa5e014eef3c578e40d0ef0948183e8a314a5a
PratamaVin7 <kevinpratamaoke@gmail.com>
2022-06-16 12:00:44 +0700

    minor updates

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -3,82 +3,82 @@ const assert = require('assert');
 Feature('Liking Restaurants');
 
 Before((I) => {
-    I.amOnPage('/#/favorite');
+  I.amOnPage('/#/favorite');
 });
 
 Scenario('showing empty favorite restaurant', (I) => {
-    I.seeElement('#restaurantsList');
-    I.see("You don't have any favorite restaurant", '#restaurantsList');
+  I.seeElement('#restaurantsList');
+  I.see("You don't have any favorite restaurant", '#restaurantsList');
 });
 
 Scenario('liking one of the restaurants', async (I) => {
-    I.see("You don't have any favorite restaurant", '#restaurantsList');
+  I.see("You don't have any favorite restaurant", '#restaurantsList');
 
-    I.amOnPage('/');
+  I.amOnPage('/');
 
-    I.seeElement('.list-item__title a');
-    const firstRestaurant = locate('.list-item__title a').first();
-    const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
-    I.click(firstRestaurant);
+  I.seeElement('.list-item__title a');
+  const firstRestaurant = locate('.list-item__title a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
 
-    I.seeElement('#likeButton');
-    I.click('#likeButton');
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-    I.amOnPage('/#/favorite');
-    I.seeElement('.list');
-    const likedRestaurantTitle = await I.grabTextFrom('.list-item__title a');
+  I.amOnPage('/#/favorite');
+  I.seeElement('.list');
+  const likedRestaurantTitle = await I.grabTextFrom('.list-item__title a');
 
-    assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
 });
 
 Scenario('cancel liking the restaurant', async (I) => {
-    I.see("You don't have any favorite restaurant", '#restaurantsList');
+  I.see("You don't have any favorite restaurant", '#restaurantsList');
 
-    I.amOnPage('/');
+  I.amOnPage('/');
 
-    I.seeElement('.list-item__title a');
-    const firstRestaurant = locate('.list-item__title a').first();
-    const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
-    I.click(firstRestaurant);
+  I.seeElement('.list-item__title a');
+  const firstRestaurant = locate('.list-item__title a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
 
-    I.seeElement('#likeButton');
-    I.click('#likeButton');
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-    I.amOnPage('/#/favorite');
-    I.seeElement('.list');
-    const likedRestaurantTitle = await I.grabTextFrom('.list-item__title a');
-    assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+  I.amOnPage('/#/favorite');
+  I.seeElement('.list');
+  const likedRestaurantTitle = await I.grabTextFrom('.list-item__title a');
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
 
-    I.click(likedRestaurantTitle);
+  I.click(likedRestaurantTitle);
 
-    I.seeElement('#likeButton');
-    I.click('#likeButton');
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-    I.amOnPage('/#/favorite');
-    I.seeElement('#restaurantsList');
-    const noFavoriteRestaurant = await I.grabTextFrom('#restaurantsList');
+  I.amOnPage('/#/favorite');
+  I.seeElement('#restaurantsList');
+  const noFavoriteRestaurant = await I.grabTextFrom('#restaurantsList');
 
-    assert.strictEqual(noFavoriteRestaurant, "You don't have any favorite restaurant");
+  assert.strictEqual(noFavoriteRestaurant, "You don't have any favorite restaurant");
 });
 
 Scenario('add a review in the customer review form', async (I) => {
-    I.see("You don't have any favorite restaurant", '#restaurantsList');
+  I.see("You don't have any favorite restaurant", '#restaurantsList');
 
-    I.amOnPage('/');
+  I.amOnPage('/');
 
-    I.seeElement('.list-item__title a');
-    I.click(locate('.list-item__title a').first());
+  I.seeElement('.list-item__title a');
+  I.click(locate('.list-item__title a').first());
 
-    I.seeElement('.review_form form');
+  I.seeElement('.review_form form');
 
-    const contentsReview = 'Makanannya Enak!';
-    I.fillField('Input Name', 'Kevin');
-    I.fillField('Input Review', contentsReview);
+  const contentsReview = 'Makanannya Enak!';
+  I.fillField('Input Name', 'Kevin');
+  I.fillField('Input Review', contentsReview);
 
-    I.click('#submit-review');
+  I.click('#submit-review');
 
-    const latestReview = locate('.customer_review').last();
-    const latestContentsReview = await I.grabTextFrom(latestReview);
+  const latestReview = locate('.customer_review').last();
+  const latestContentsReview = await I.grabTextFrom(latestReview);
 
-    assert.strictEqual(contentsReview, latestContentsReview);
+  assert.strictEqual(contentsReview, latestContentsReview);
 });
